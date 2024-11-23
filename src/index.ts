88e7b1fc02d6eaf7adf1741e8992e409504bf159
lppellin <lucaspaolopellin@gmail.com>
2024-11-23 15:14:28 -0300

    feat: adiciona classe arma

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import Arma from "./classes/Arma";
+
+const repulsor  = new Arma('Repulsores', 'repulsores de energia do Iron Man', 30)
+const martelo = new Arma('Mjolnir', 'martelo do Thor', 50)
+const escudo = new Arma('Escudo', 'escudo do Capitão América', 20)
+
+
+
